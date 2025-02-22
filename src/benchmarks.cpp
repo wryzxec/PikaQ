4647af322133f8ea5b1e41c78436bc2add9352a7
wryzxec <lucas.bruckbauer@gmail.com>
2025-02-22 01:20:26 +0000

    refactor(benchmark): benchmarking template

--- src/benchmarks.cpp
+++ src/benchmarks.cpp
@@ -5,87 +5,59 @@
 #include <thread>
 
 constexpr int QUEUE_SIZE = 1000000;
-constexpr int NUM_ITERS = 10'000;
+constexpr int NUM_ITERS = 50000;
 
-Pika_Q<int> custom_q(QUEUE_SIZE);
+template <typename QueueType, typename PushFunc, typename PopFunc>
+static void spsc_queue_benchmark(benchmark::State& state, QueueType& queue, PushFunc push, PopFunc pop) {
+    for (auto _ : state) {
+        std::thread prod([&queue, push] {
+            for (int i = 0; i < NUM_ITERS; ++i) {
+                while (!push(queue, i)) {
+                }
+                benchmark::DoNotOptimize(i);
+            }
+        });
 
-static void custom_q_benchmark(benchmark::State& state) {
-  for (auto _ : state) {
-    std::thread prod([] {
-      for (int i = 0; i < NUM_ITERS; ++i) {
-        while (!custom_q.push(i));
-        benchmark::DoNotOptimize(i);  
-      }
-    });
+        std::thread cons([&queue, pop] {
+            int val{};
+            for (int i = 0; i < NUM_ITERS; ++i) {
+                while (!pop(queue, val)) {
+                }
+                benchmark::DoNotOptimize(val);
+            }
+        });
 
-    std::thread cons([] {
-      int val{};
-      for (int i = 0; i < NUM_ITERS; ++i) {
-        while (!custom_q.pop(val));
-        benchmark::DoNotOptimize(val);
-      }
-    });
+        prod.join();
+        cons.join();
 
-    prod.join();
-    cons.join();
-
-    state.SetItemsProcessed(NUM_ITERS);
-  }
+        state.SetItemsProcessed(NUM_ITERS);
+        state.counters["Throughput"] = benchmark::Counter(NUM_ITERS, benchmark::Counter::kIsRate);
+    }
 }
-BENCHMARK(custom_q_benchmark)->Iterations(NUM_ITERS)->Unit(benchmark::kMillisecond);
+
+Pika_Q<int, QUEUE_SIZE> custom_q;
+static void pika_q_benchmark(benchmark::State& state) {
+    spsc_queue_benchmark(state, custom_q, 
+        [](auto& q, int val) { return q.push(val); },
+        [](auto& q, int& val) { return q.pop(val); });
+}
+BENCHMARK(pika_q_benchmark)->Unit(benchmark::kMillisecond);
 
 boost::lockfree::spsc_queue<int> boost_q(QUEUE_SIZE);
-
-static void boost_spsc_benchmark(benchmark::State& state) {
-  for (auto _ : state) {
-    std::thread prod([] {
-      for (int i = 0; i < NUM_ITERS; ++i) {
-        while (!boost_q.push(i));
-        benchmark::DoNotOptimize(i);
-      }
-    });
-
-    std::thread cons([] {
-      int val{};
-      for (int i = 0; i < NUM_ITERS; ++i) {
-        while (!boost_q.pop(val));
-        benchmark::DoNotOptimize(val);
-      }
-    });
-
-    prod.join();
-    cons.join();
-    
-    state.SetItemsProcessed(NUM_ITERS);
-  }
+static void boost_q_benchmark(benchmark::State& state) {
+    spsc_queue_benchmark(state, boost_q,
+        [](auto& q, int val) { return q.push(val); },
+        [](auto& q, int& val) { return q.pop(val); });
 }
-BENCHMARK(boost_spsc_benchmark)->Iterations(NUM_ITERS)->Unit(benchmark::kMillisecond);
+BENCHMARK(boost_q_benchmark)->Unit(benchmark::kMillisecond);
 
 folly::ProducerConsumerQueue<int> folly_q(QUEUE_SIZE);
-
 static void folly_q_benchmark(benchmark::State& state) {
-  for (auto _ : state) {
-    std::thread prod([] {
-      for (int i = 0; i < NUM_ITERS; ++i) {
-        while (!folly_q.write(i));
-        benchmark::DoNotOptimize(i);  
-      }
-    });
-
-    std::thread cons([] {
-      int val{};
-      for (int i = 0; i < NUM_ITERS; ++i) {
-        while (!folly_q.read(val));
-        benchmark::DoNotOptimize(val);
-      }
-    });
-
-    prod.join();
-    cons.join();
-
-    state.SetItemsProcessed(NUM_ITERS);
-  }
+    spsc_queue_benchmark(state, folly_q,
+        [](auto& q, int val) { return q.write(val); },
+        [](auto& q, int& val) { return q.read(val); });
 }
-BENCHMARK(folly_q_benchmark)->Iterations(NUM_ITERS)->Unit(benchmark::kMillisecond);
+BENCHMARK(folly_q_benchmark)->Unit(benchmark::kMillisecond);
 
 BENCHMARK_MAIN();
+
